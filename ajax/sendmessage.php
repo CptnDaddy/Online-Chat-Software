2b777a65b23a522c1614249e504af75d986f7d10
Termincat <dohurensohn@yahoo.de>
2017-11-06 12:37:11 +0100

    send messages

--- ajax/sendmessage.php
+++ ajax/sendmessage.php
@@ -1,8 +1,18 @@
 <?php 
-session_start();
-if (isset($_POST['chattext'])) {
-    echo "lol\n" . $_POST['chattext'];
-}
+    require("lib/mysql_lib.php");
+    
+    $mysql = new mysql_lib();
+    $mysql->connectToDatabase();
+    
+    session_start();
+    
+    if (isset($_POST['chattext'])) {
+        $stmt = $mysql->getConnection()->prepare("");
+        $stmt->bind_param("sii", $_POST["chattext"], $_SESSION['userid'], 1);
+        $stmt->execute();
+        $stmt->close();
+        echo "success";
+    }
 
 
 
