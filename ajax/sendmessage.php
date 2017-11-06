213c4a02ee7b9e48e346f981ba6ee7f2258ba800
Termincat <dohurensohn@yahoo.de>
2017-11-06 12:41:30 +0100

    Fix x2

--- ajax/sendmessage.php
+++ ajax/sendmessage.php
@@ -7,7 +7,7 @@
     session_start();
     
     if (isset($_POST['chattext'])) {
-        $stmt = $mysql->getConnection()->prepare("");
+        $stmt = $mysql->getConnection()->prepare("insert into message (text, fk_user, fk_room) values (?, ?, ?)");
         $stmt->bind_param("sii", $_POST["chattext"], $_SESSION['userid'], 1);
         $stmt->execute();
         $stmt->close();
