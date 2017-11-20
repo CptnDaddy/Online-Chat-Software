486c22594b3c9a6e915d9404fcb8a9805c7c9a3c
TheOneAndOnlyStressica <keucher.jessica@gmail.com>
2017-11-20 12:48:36 +0100

    new function chatlib

--- lib/chatlib.php
+++ lib/chatlib.php
@@ -26,5 +26,11 @@ function listRooms($con) {
     
     $stmt->close();
 }
+function getRandomWord($len) {
+    $word = array_merge(range('a', 'z'), range('A', 'Z'));
+    shuffle($word);
+    return substr(implode($word), 0, $len);
+}
+
 
 ?>
