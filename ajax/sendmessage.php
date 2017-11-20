c80d11b48eb00c3e09c414bdbf65ffc98b6dabfb
CptnDaddy <nicolas.hage2@gmail.com>
2017-11-20 08:59:01 +0100

    Fixed room switch and message sending

--- ajax/sendmessage.php
+++ ajax/sendmessage.php
@@ -8,10 +8,12 @@
     
     if (isset($_POST['chattext'])) {
         $stmt = $mysql->getConnection()->prepare("insert into message (text, fk_user, fk_room) values (?, ?, ?)");
-        $stmt->bind_param("si", $_POST['chattext'], $_SESSION['userid']);
+        $stmt->bind_param("sii", $_POST['chattext'], $_SESSION['userid'], $_SESSION['roomid']);
         $stmt->execute();
         $stmt->close();
         echo "success";
+    } else {
+    	echo "failed";
     }
 
 
