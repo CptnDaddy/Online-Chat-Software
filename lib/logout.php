358780f8e7fafcb2e37474e7e2f446c96737793d
Termincat <dohurensohn@yahoo.de>
2017-11-27 10:19:27 +0100

    i dont know

--- lib/logout.php
+++ lib/logout.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
+require("../lib/mysql_lib.php");
+
+$mysql = new mysql_lib();
+$mysql->connectToDatabase();
+$stmt = $mysql->getConnection()->prepare("update users set fk_room = 'NULL' where id = ?");
+$stmt->bind_param("i", $_SESSION['userid']);
+$stmt->execute();
+$stmt->close();
 session_unset();
 session_destroy();
-
 header("Location: ../index.php?s=5");
 ?>
