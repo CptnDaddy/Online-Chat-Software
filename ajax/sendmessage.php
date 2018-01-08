87d364df900382308fc4108ef9e4c8e6ed9231af
Termincat <dohurensohn@yahoo.de>
2018-01-08 09:02:23 +0100

    filter for empty schtrings

--- ajax/sendmessage.php
+++ ajax/sendmessage.php
@@ -7,14 +7,20 @@
     session_start();
     
     if (isset($_POST['chattext'])) {
-        $stmt = $mysql->getConnection()->prepare("insert into message (text, fk_user, fk_room) values (?, ?, ?)");
-        $stmt->bind_param("sii", $_POST['chattext'], $_SESSION['userid'], $_SESSION['roomid']);
-        $stmt->execute();
-        $stmt->close();
-        echo "success";
+        $message = strip_tags($_POST['chattext']);
+        if( !(ctype_space($message)) && !(empty($message))){
+            $stmt = $mysql->getConnection()->prepare("insert into message (text, fk_user, fk_room) values (?, ?, ?)");
+            $stmt->bind_param("sii", $message, $_SESSION['userid'], $_SESSION['roomid']);
+            $stmt->execute();
+            $stmt->close();
+            echo "success";
+        } else {
+            echo "failed";
+        }
     } else {
     	echo "failed";
     }
+    
 
 
 
