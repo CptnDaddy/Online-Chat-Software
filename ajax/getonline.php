9f37f4fee9e5040f6c716bc9ab378132ba5c1aed
CptnDaddy <nicolas.hage2@gmail.com>
2018-01-29 09:11:35 +0100

    Onlinelist profile picture fix

--- ajax/getonline.php
+++ ajax/getonline.php
@@ -7,7 +7,7 @@
 	
 	session_start();
 	if(isset($_GET['r'])) {
-		$stmt = $mysql->getConnection()->prepare("select username from users where fk_room = ?");
+		$stmt = $mysql->getConnection()->prepare("select username,haspic from users where fk_room = ?");
 		$stmt->bind_param("i", $_GET['r']);
 		$stmt->execute();
 		if($stmt->field_count > 0) {
@@ -16,7 +16,11 @@
 			$result = $stmt->get_result();
 			while ($row = $result->fetch_array(MYSQLI_BOTH)) {
 				echo '<li class="collection-item avatar">';
+				if($row['haspic']) {
+					echo '<img src="uploads/' . $row['username'] . '.png" alt class="circle" height="100px">';
+				} else {
 					echo '<img src="img/default.png" alt class="circle" height="100px">';
+				}
 					echo '<span class="title">' . $row['username'] . '</span>';
 				echo '</li>';
 			}
